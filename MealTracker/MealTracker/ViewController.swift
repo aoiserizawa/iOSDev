94f43769ba4ff38e351fad080be1af54b06ce0dc
Luisito Yumang <alvinvaldez@Luisitos-Mac-mini.local>
2016-02-26 05:13:06 +0800

    added a star rating

--- MealTracker/MealTracker/ViewController.swift
+++ MealTracker/MealTracker/ViewController.swift
@@ -1,35 +1,34 @@
 //
 //  ViewController.swift
-//  MealTracker
+//  FoodTracker
 //
-//  Created by Luisito Yumang on 2/24/16.
-//  Copyright © 2016 Alvin Joseph Valdez. All rights reserved.
+//  Created by Jane Appleseed on 5/23/15.
+//  Copyright © 2015 Apple Inc. All rights reserved.
+//  See LICENSE.txt for this sample’s licensing information.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,
-    UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-    
-    //MARK Properties
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
-    
-    
+    @IBOutlet weak var ratingControl: RatingControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
     }
     
-    //MARK: UITextFieldDelegate
+    // MARK: UITextFieldDelegate
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        //Hide the keyboard
+        // Hide the keyboard.
         textField.resignFirstResponder()
-        
         return true
     }
     
@@ -37,43 +36,39 @@ class ViewController: UIViewController, UITextFieldDelegate,
         mealNameLabel.text = textField.text
     }
     
-    //MARK: Actions
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        self.textFieldDidEndEditing(nameTextField)
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
-    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         
-        //Hide the keyboard
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
         nameTextField.resignFirstResponder()
         
         // UIImagePickerController is a view controller that lets a user pick media from their photo library.
         let imagePickerController = UIImagePickerController()
         
+        // Only allow photos to be picked, not taken.
         imagePickerController.sourceType = .PhotoLibrary
         
+        // Make sure ViewController is notified when the user picks an image.
         imagePickerController.delegate = self
         
         presentViewController(imagePickerController, animated: true, completion: nil)
     }
-    
-    // MARK: UIImagePickerControllerDelegate
-    
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        
-        // The info dictionary contains multiple representations of the image, and this uses the original.
-        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
-        // Set photoImageView to display the selected image
-        photoImageView.image = selectedImage
-        
-        // Dismiss the picker
-        
-        dismissViewControllerAnimated(true, completion: nil)
-    }
+
 }
 
